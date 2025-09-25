8df08c3e8fce4adad618785fca5878679fa2e4cf
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-09-25 16:30:43 -0400

    Commit

--- src/main/java/com/pluralsight/StringFormat.java
+++ src/main/java/com/pluralsight/StringFormat.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class StringFormat {
+    public static void main(String[] args) {
+        String name = "Alice";
+        int age = 25;
+        double gpa = 3.6789;
+        String message1 = String.format("Name: is %s", name);
+        String message2 = String.format("Age: %d", age);
+        String message3 = String.format("gpa: %d", gpa);
+        System.out.println (message1);
+        System.out.println (message2);
+        System.out.println (message3);
+        System.out.printf("Name: %s, Age: %d,gpa: %.4d");
+
+
+
+    }
+
+
 }
